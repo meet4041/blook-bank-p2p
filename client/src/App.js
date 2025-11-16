9f66cda6166356a77dc1efdcdd765adb36fb206a
Meet Gandhi <meetgandhi4041@gmail.com>
2025-11-16 16:42:44 +0530

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import SearchDonors from "./pages/SearchDonors";
+import Requests from "./pages/Requests";
+import IncomingRequests from "./pages/IncomingRequests";
+import VerifyHospitals from "./pages/VerifyHospitals";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/search"
+          element={
+            <ProtectedRoute>
+              <SearchDonors />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/requests"
+          element={
+            <ProtectedRoute>
+              <Requests />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/incoming"
+          element={
+            <ProtectedRoute>
+              <IncomingRequests />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/verify-hospitals"
+          element={
+            <ProtectedRoute>
+              <VerifyHospitals />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
